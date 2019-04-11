2ead4f94dd1bb7ad5a8d41750013c027ed09e685
fazlicodes <fazlimaam@gmail.com>
2019-04-11 10:41:50 +0530

    fazli

---
+++ fazliclass2.java
@@ -0,0 +1,13 @@
+
+public class fazliclass2 {
+
+	public fazliclass2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
